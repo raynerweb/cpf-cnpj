d3e6253139f5dde571a79a88112b052fb375418c
rayner <raynerweb@gmail.com>
2016-07-26 21:40:49 -0300

    add atributo as-number na diretiva

--- cpj-cnpj.directive.js
+++ cpj-cnpj.directive.js
@@ -5,26 +5,34 @@
     .directive('cpfCnpj', cpfCnpj);
   function cpfCnpj(){
     return {
-      link : link
+      link : link,
+      require: 'ngModel'
     };
-    function link(scope, element, attr){
-        attr.$set('maxlength',18);
-        element.on('keypress', applyMask);
-        element.on('keyup', applyMask);
-        function applyMask(){
-          var value=element.val().replace(/\D/g,"");
-          if (value.length <= 11) {
-            value=value.replace(/(\d{3})(\d)/,"$1.$2");
-            value=value.replace(/(\d{3})(\d)/,"$1.$2");
-            value=value.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
-          } else {
-            value=value.replace(/^(\d{2})(\d)/,"$1.$2")
-            value=value.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3");
-            value=value.replace(/\.(\d{3})(\d)/,".$1/$2");
-            value=value.replace(/(\d{4})(\d)/,"$1-$2");
-          }
-          element.val(value);
-        };
-    };
-  }
+    function link(scope, element, attrs, ngModelController){
+      attrs.$set('maxlength',18);
+      scope.$watch(attrs['ngModel'], applyMask);
+      function applyMask(event){
+        var value=element.val().replace(/\D/g,"");
+        if (value.length <= 11) {
+          value=value.replace(/(\d{3})(\d)/,"$1.$2");
+          value=value.replace(/(\d{3})(\d)/,"$1.$2");
+          value=value.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
+        } else {
+          value=value.replace(/^(\d{2})(\d)/,"$1.$2")
+          value=value.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3");
+          value=value.replace(/\.(\d{3})(\d)/,".$1/$2");
+          value=value.replace(/(\d{4})(\d)/,"$1-$2");
+        }
+        element.val(value);
+        if('asNumber' in attrs){
+          ngModelController.$setViewValue(
+            isNaN(parseInt(value.replace(/\D/g,""), 10))
+            ? undefined
+            : parseInt(value.replace(/\D/g,""), 10));
+        } else {
+          ngModelController.$setViewValue(value);
+        }
+      }
+    };//link
+  };//cpfCnpj
 })();
